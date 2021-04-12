87ecff9430710ca675614feca219c6ac32eda28e
Vinayakatp <vinayakatp10@gmail.com>
2021-04-12 20:16:59 +0530

    my second commit

--- testCases/test_SearchCustomer.py
+++ testCases/test_SearchCustomer.py
@@ -43,3 +43,33 @@ class Test_SearchCustomerByEmail_004:
         self.driver.close()
         assert True == status
         self.logger.info("***************  TC_SearchCustomerByEmail_004 Finished  *********** ")
+
+        def test_searchCustomerByEmail(self, setup):
+            self.logger.info("************* SearchCustomerByEmail_004 **********")
+            self.driver = setup
+            self.driver.get(self.baseURL)
+            self.driver.maximize_window()
+
+            self.lp = LoginPage(self.driver)
+            self.lp.set_email(self.username)
+            self.lp.set_password(self.password)
+            self.lp.click_login()
+            self.logger.info("************* Login succesful **********")
+
+            self.logger.info("******* Starting Search Customer By Email **********")
+
+            self.homePage = CustomerPage(self.driver)
+            self.homePage.clickOnCustomerMainMenu().click()
+            time.sleep(3)
+            self.homePage.clickOnCustomerSubMenu().click()
+            time.sleep(3)
+
+            self.logger.info("************* searching customer by emailID **********")
+            searchcust = SearchCustomer(self.driver)
+            searchcust.setEmail("victoria_victoria@nopCommerce.com")
+            searchcust.clickSearch()
+            time.sleep(5)
+            status = searchcust.searchCustomerByEmail("victoria_victoria@nopCommerce.com")
+            self.driver.close()
+            assert True == status
+            self.logger.info("***************  TC_SearchCustomerByEmail_004 Finished  *********** ")
