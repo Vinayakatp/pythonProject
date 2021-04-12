48666d00f6fa3eb2cd88e01d2bb22d16854c4acd
Vinayakatp <64779299+Vinayakatp@users.noreply.github.com>
2021-04-12 22:26:26 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 rem chrome
-pytest -v -m "sanity" --html =./Reports/report.html testCases/ --browser firefox
+pytest -v -m "sanity" --html =./Reports/report.html testCases/ --browser chrome
 rem pytest -v -m "sanity or regression" --html =./Reports/report.html testCases/ --browser firefox
 rem pytest -v -m "sanity or regression" --html =./Reports/report.html testCases/ --browser chrome
 rem pytest -v -m "sanity and regression" --html =./Reports/report.html testCases/ --browser chrome
